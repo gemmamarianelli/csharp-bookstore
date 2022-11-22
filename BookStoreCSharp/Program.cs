5dfe6366c7d9caecd5d24bffaa50483b5cfe419e
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-22 15:57:37 +0100

    primo push

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var nomeLibro = "Noi Siamo Infinito";
+var autore = "Stephen Chosby";
+var ISBN = 8820068451;
+var NumeroPagine = 278;
+var pesoLibro = 440;
+int larghezza = 14;
+int profondità = 2;
+var lunghezza = 22;
+var valutazioni = 423;
+var valutazioneMedia = 5;
+var kindle = "si";
+var copertinaFlessibile = "si";
+
+
+
+
+
+Console.WriteLine("----IL LIBRO DI OGGI E': " + nomeLibro + "di" +autore);
+Console.WriteLine("ISBN: " + ISBN);
+Console.WriteLine("Numero delle pagine: " + NumeroPagine);
+Console.WriteLine("Peso Libro: " + pesoLibro);
+
+
+
+Console.WriteLine("largezza: " + larghezza);
+Console.WriteLine("profondità: "+ profondità);
+Console.WriteLine("lunghezza: " + lunghezza);
+Console.WriteLine("valutazione: " + valutazioni);
+Console.WriteLine("Media stella: " + valutazioneMedia);
+Console.WriteLine("Versione Kindle: " + kindle);
+Console.WriteLine("Copertina Flessibile: " + copertinaFlessibile);
+Console.WriteLine("---------------------------------------------");
